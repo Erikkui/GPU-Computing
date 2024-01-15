902f6ed096850e9a3842464d5e42a04f34980e7d
Erikkui <erik.kuitunen@outlook.com>
2024-01-16 01:43:47 +0200

    (Somewhat?) completed ex1 task 3

--- testi.py
+++ testi.py
@@ -1,73 +1,58 @@
-import numpy as np
-import time
-# import pycuda stuff
-import pycuda.driver as cuda
-import pycuda.autoinit
-from pycuda.compiler import SourceModule
-
-modd = SourceModule("""
-    __global__ void matmul(int n, const float *A, const float *B, float *C){
-
-    int tx = threadIdx.x;
-    int ty = threadIdx.y;
-
-    int bx = blockIdx.x;
-    int by = blockIdx.y;
-
-    int row = by*blockDim.y + ty;
-    int col = bx*blockDim.x + tx;
-
-    if(row < n && col < n){
-        float val = 0.0;
-        for(int i=0; i<n; ++i){
-        val += A[row*n + i]*B[n*i + col];
-        }
-        C[row*n + col] = val;
-    }
-    }
-""")
-
-BLOCK_SIZE = 16
-
-n = 4
-ni = np.int32(n)
-
-# matrix A 
-a = np.random.randn(n, n)*100
-a = a.astype(np.float32)
-
-
-# matrix B
-b = np.random.randn(n, n)*100
-b = b.astype(np.float32)
-
-# matrix B
-c = np.empty([n, n])
-c = c.astype(np.float32)
-
-# allocate memory on device
-a_gpu = cuda.mem_alloc(a.nbytes)
-b_gpu = cuda.mem_alloc(b.nbytes)
-c_gpu = cuda.mem_alloc(c.nbytes)
-
-# copy matrix to memory
-cuda.memcpy_htod(a_gpu, a)
-cuda.memcpy_htod(b_gpu, b)
-
-# get function
-matmul = modd.get_function("matmul");
-
+kernel_code_template = """
+  __global__ void matmul_sharedmem( const float* A, const float* B, float* C, int M, int N, int K, int n_tiles) {
     
-
+    float C_elem = 0;
     
-# call gpu function
+    // Block indices, each block computes submatrix of C, C_sub
+    int block_row = blockIdx.y;
+    int block_col = blockIdx.x;
+    
+    // Thread indices. Each thread computes an element of C_sub
+    int thread_row = threadIdx.y;
+    int thread_col = threadIdx.x;
+    
+    // Determining number of tiles for loop
+    if ( round( N / %(TILE_WIDTH)s ) * %(TILE_WIDTH)s < N ) {    
+        int n_tiles = round( N / %(TILE_WIDTH)s ) + 1;
+    } else {
+        int n_tiles = round( N / %(TILE_WIDTH)s );
+    }
 
-matmul(ni, a_gpu, b_gpu, c_gpu, block=(BLOCK_SIZE,BLOCK_SIZE,1), grid=(1,1,1));
-
-# copy back the result
-cuda.memcpy_dtoh(c, c_gpu)
-
-print (np.linalg.norm(c - np.dot(a,b)) )
-print (c)
-print (np.dot(a,b))
-print (c - np.dot(a,b))
+    // Looping through relevant submatrices to compute C_sub
+    for (int ii = 0; ii < n_tiles; ++ii ) { 
+    
+      // Loading submatrices from global memory
+      int linear_ind_A = N * thread_row + thread_col + ii * %(TILE_WIDTH)s + block_row * %(TILE_WIDTH)s * N;
+      int linear_ind_B = K * thread_row + thread_col + ii * %(TILE_WIDTH)s * K + block_col * %(TILE_WIDTH)s;
+      
+      // Shared memory for the submatrices of A and B
+      __shared__ float A_sub[ %(TILE_WIDTH)s ][ %(TILE_WIDTH)s ];
+      __shared__ float B_sub[ %(TILE_WIDTH)s ][ %(TILE_WIDTH)s ];
+      
+      // TODO check for threads outside matrix bounds
+      
+      A_sub[ thread_row ][ thread_col ] = A[ linear_ind_A ];
+      B_sub[ thread_row ][ thread_col ] = B[ linear_ind_B ];
+      
+      __syncthreads();
+      
+      // Doin the actual multiplication of the submatrices
+      for (int kk = 0; kk < %(TILE_WIDTH)s; ++kk) {
+        
+        float A_sub_elem = A_sub[ thread_row ][ kk ];
+        float B_sub_elem = B_sub[ kk ][ thread_col ];
+        
+        C_elem += A_sub_elem * B_sub_elem;
+        
+      }
+      
+      __syncthreads();
+    
+    }
+      
+    // Saving the C_elem to matrix C
+    int linear_ind_C = block_row * %(TILE_WIDTH)s * K + block_col * %(TILE_WIDTH)s + thread_row * K + thread_col;
+    C[ linear_ind_C ] = C_elem;
+    
+  } 
+"""
